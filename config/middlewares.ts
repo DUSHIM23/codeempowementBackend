de3b032b60a11bc3199067e8756410401e0024c6
DUSHIME1212 <mdonavan33@gmail.com>
2024-12-30 07:45:07 +0200

    Add initial API structure for blog, gallery, and project components

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,21 @@
 export default [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
